656920dd7af2bd32c935b19c7dbee445cf27210f
Eisha Fatima Shah <140654347+k200383@users.noreply.github.com>
2025-06-08 04:18:53 +0500

    Update trigger_pipelines.py

--- trigger_pipelines.py
+++ trigger_pipelines.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 tenant_id = "a1e3cc4f-47e2-4e32-a7a1-5b14136b160b"
 client_id = "4d116976-162f-4038-996e-06379e28940d"
-client_secret = os.getenv("AZURE_CLIENT_SECRET")
+client_secret = os.getenv("FABRIC_CLIENT_SECRET")
 workspace_id = "4b128012-36fa-4785-9f9b-bf62d6e5a3bb"
 pipeline_id = "332993a1-6150-4cc3-96ff-d2ccd2dd6094"
 
